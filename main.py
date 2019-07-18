15d5d0923ded439b6161c9cce820f9b7161fd68b
Moleque <molecada@yandex.ru>
2019-07-18 14:22:46 +0300

    Fixed tiles size

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import sys
 import os
 import shutil
+import re
 
 HEADER = "osm_100-"   
+
 
 def convert(root = "tiles", mapId = "1", dpi = "l"):
     tree = os.walk(root)
     for folder in tree:
         if len(folder[2]) > 0:
-            path = HEADER + dpi + "-" + mapId
-            path += folder[0].replace(root, "").replace("\\0", "").replace("\\z", "-").replace("\\x", "-")
+            path = HEADER + dpi + "-" + mapId + "-"
+            values = re.findall(r'\w(\d+)\\0', folder[0])
+            path += str(int(values[0]) - 1) + "-" + values[1]
             for tile in folder[2]:
                 filename = path + tile.replace("y", "-")
                 os.rename(folder[0] + "\\" + tile, root + "\\" + filename)
@@ -18,6 +21,14 @@ def convert(root = "tiles", mapId = "1", dpi = "l"):
 
 
 if __name__ == "__main__":
-    root = sys.argv[1]
-    mapId = sys.argv[2]
-    convert(root, mapId)
+    root = "tiles"
+    mapId = "1"
+    dpi = "l"
+    
+    if sys.argv[1]:
+        root = sys.argv[1]
+    if sys.argv[2]:
+        mapId = sys.argv[2]
+    if sys.argv[3]:
+        dpi = sys.argv[3]
+    convert(root, mapId, dpi)
